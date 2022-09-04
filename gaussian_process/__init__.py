08ddc5c3788899aec7313bedb069c095ea860a82
H-Joker <983117365@qq.com>
2022-09-04 20:48:24 +0800

    Update

--- gaussian_process/__init__.py
+++ gaussian_process/__init__.py
@@ -1,5 +1,1 @@
-from gaussian_process.kernel import RBFKernel
-from gaussian_process.gaussian_process_regression import GaussianProcessRegressor
 
-__all__ = ['RBFKernel',
-           'GaussianProcessRegressor']
--- prob_distribution/__init__.py
+++
@@ -1,9 +0,0 @@
-from prob_distribution.random_variable import RandomVariable
-from prob_distribution.gaussian import Gaussian
-from prob_distribution.gamma import Gamma
-
-__all__ = [
-    "RandomVariable",
-    "Gaussian",
-    "Gamma"
-]
